aae41737f80990bd6a891f5236cebabf0ce60df4
Isaac7app <ik.appiagyei@gmail.com>
2022-02-22 10:40:13 +0100

    router is working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -20,6 +23,14 @@ const App = () => {
   //Fetch Data
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data;
+  }
+
+   //Fetch a single Data
+   const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     return data;
@@ -50,24 +61,48 @@ const deleteTask = async (id) =>{
 }
 
 // Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle,
+  reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
  setTasks(
    tasks.map((task) => task.id === id ?
-    { ...task, reminder: !task.reminder} : task))
+    { ...task, reminder: data.reminder} : task))
 }
  
   return (
+    <Router>
       <div className='container'>
        <Header 
        onAdd={() => setShowAddTask(!showAddTask)}
        showAdd={showAddTask}
        />
-      {showAddTask && <AddTask onAdd={addTask} />}
-       {tasks.length > 0 ? 
-       (<Tasks tasks={tasks} 
-       onDelete={deleteTask} onToggle={toggleReminder} />): 
-       ('No Reminder To Show')}
+       
+       <Route path='/' exact render={(props) => (
+         <>
+         {showAddTask && <AddTask onAdd={addTask} />}
+          {tasks.length > 0 ? 
+         (<Tasks tasks={tasks} 
+           onDelete={deleteTask} onToggle={toggleReminder} />): 
+            ('No Reminder To Show')}
+         </>
+       )} 
+       />
+       <Route path='/about' component={About} />
+       <Footer />
       </div>
+      </Router>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
+
   return (
     <header className='header'>
-        <h1>{title}</h1>
-        <Button 
-        color={showAdd ? 'red' : 'green'} 
-        text={ showAdd ? 'Close' : 'Add' }
-         onClick={onAdd}/>
-       
+      <h1>{title}</h1>
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+     
     </header>
   )
 }
 
-
-
 Header.defaultProps = {
-    title: 'Task Reminder',
+  title: 'Task Tracker',
 }
 
-Header.propType = {
-    title: PropTypes.string.isRequired,
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
-
 export default Header
